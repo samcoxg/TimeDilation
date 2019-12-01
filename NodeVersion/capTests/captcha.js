bd56845ebd58b59f39587697dfdb8d7a20051899
samcoxg <50882390+samcoxg@users.noreply.github.com>
2019-11-30 17:05:56 -0700

    question fixes

--- NodeVersion/capTests/captcha.js
+++ NodeVersion/capTests/captcha.js
@@ -1,5 +1,3 @@
-
-
 var Questions = [
     [1, "Select the word that best describes this image. <br><br><img src='images/label_image/1.jpg' style='height:300px;'>", "a", "frog", "lizard", "cat", "camelion", "alpacha", "snake", "eal", "fish", 1],
     [2, "Select the word that best describes this image. <br><br><img src='images/label_image/2.jpg' style='height:300px;'>", "b", "truck", "car", "train", "bus", "van", "plane", "ATV", "tractor", 1],
@@ -38,10 +36,10 @@ var Questions = [
     [35, "Type out the numbers and letters in the image below. <br><br><img src='images/typeWords/11.jpg' style='width:200px;'>", "e", "bkngkp", "pkn9kp", "bkn9pk", "bnk9kp", "bkn9kp", "bkn99p", "pkn9kp", "bkm9kp", 3],
     [36, "Type out the numbers and letters in the image below. <br><br><img src='images/typeWords/12.jpg' style='width:200px;'>", "f", "y6g9nf", "y5g9mf", "y5g9fn", "y5n9fg", "y5n9fl", "y5g9nf", "y5g99f", "y5g9yf", 3],
     [37, "Type out the numbers and letters in the image below. <br><br><img src='images/typeWords/13.jpg' style='width:200px;'>", "g", "w86hp", "w68ph", "vv68hp", "uu80hp", "tv68hp", "v68hp", "w68hp", "w98hp", 3],
-    [38, "Which of the following images contains a dog?", 'images/dogs/21.jpg','images/dogs/20.jpg','images/dogs/1.jpg','images/dogs/16.jpg', "images/random/1/jpg", "images/random/2/jpg", "images/random/3/jpg", "images/random/4/jpg", "c", 4],
-    [39, "Which of the following images contains a dog?", 'images/dogs/9.jpg','images/dogs/2.jpg','images/dogs/10.jpg','images/dogs/11.jpg', "images/random/5/jpg", "images/random/6/jpg", "images/random/7/jpg", "images/random/8/jpg", "b", 4],
-    [40, "Which of the following images contains a dog?", 'images/dogs/3.jpg','images/dogs/15.jpg','images/dogs/14.jpg','images/dogs/13.jpg', "images/random/4/jpg", "images/random/5/jpg", "images/random/9/jpg", "images/random/10/jpg", "a", 4],
-    [41, "Which of the following images contains a dog?", 'images/dogs/12.jpg','images/dogs/21.jpg','images/dogs/9.jpg','images/dogs/4.jpg', "images/random/11/jpg", "images/random/12/jpg", "images/random/13/jpg", "images/random/14/jpg", "d", 4],
+    [38, "Which of the following images contains a dog?", 'images/dogs/21.jpg','images/dogs/20.jpg','images/dogs/1.jpg','images/dogs/16.jpg', "images/random/1.jpg", "images/random/2.jpg", "images/random/3.jpg", "images/random/4.jpg", "c", 4],
+    [39, "Which of the following images contains a dog?", 'images/dogs/9.jpg','images/dogs/2.jpg','images/dogs/10.jpg','images/dogs/11.jpg', "images/random/5.jpg", "images/random/6.jpg", "images/random/7.jpg", "images/random/8.jpg", "b", 4],
+    [40, "Which of the following images contains a dog?", 'images/dogs/3.jpg','images/dogs/15.jpg','images/dogs/14.jpg','images/dogs/13.jpg', "images/random/4.jpg", "images/random/5.jpg", "images/random/9.jpg", "images/random/10.jpg", "a", 4],
+    [41, "Which of the following images contains a dog?", 'images/dogs/12.jpg','images/dogs/21.jpg','images/dogs/9.jpg','images/dogs/4.jpg', "images/random/11.jpg", "images/random/12.jpg", "images/random/13.jpg", "images/random/14.jpg", "d", 4],
     [42, "Finish the following pattern: <br> 3, 6, 9, 12...", "c", "16", "11", "15", "18", "16", "12", "9", "14", 5],
     [43, "Finish the following pattern: <br> 2, 4, 3, 5, 4...", "f", "7", "8", "2", "3", "1", "6", "4", "5", 5],
     [44, "Finish the following pattern: <br> 10, 20, 40, 80...", "h", "90", "60", "90", "120", "14", "140", "100", "160", 5],
@@ -51,14 +49,14 @@ var Questions = [
     [48, "Choose the option that does NOT belong.", "b", "sparrow", "moose", "hawk", "eagle", "bird", "robin", "sparrow", "falcon", 6],
     [49, "Choose the option that does NOT belong.", "c", "train", "car", "piano", "bus", "truck", "plane", "jet", "bike", 6],
     [50, "Choose the option that does NOT belong.", "d", "ski", "snowboard", "snowshoe", "volcano", "snow", "ice", "cold", "winter", 6],
-    [51, "Which of the following images contains a bike?", 'images/dogs/13.jpg', 'images/dogs/11.jpg', 'images/dogs/12.jpg', 'images/dogs/10.jpg', "images/random/1/jpg", "images/random/3/jpg", "images/random/5/jpg", "images/random/7/jpg", "a", 4],
-    [52, "Which of the following images contains a bike?", 'images/dogs/18.jpg', 'images/bikes/bike1.jpg', 'images/dogs/17.jpg', 'images/dogs/16.jpg', "images/random/18/jpg", "images/random/17/jpg", "images/random/16/jpg", "images/random/15/jpg", "b",  4],
-    [53, "Which of the following images contains a bike?", 'images/dogs/3.jpg', 'images/dogs/15.jpg', 'images/bikes/bike2.jpg', 'images/dogs/5.jpg', "images/random/14/jpg", "images/random/13/jpg", "images/random/12/jpg", "images/random/11/jpg", "c",  4],
-    [54, "Which of the following images contains a bike?", 'images/dogs/12.jpg', 'images/dogs/21.jpg', 'images/dogs/17.jpg', 'images/bikes/bike3.jpg', "images/random/10/jpg", "images/random/9/jpg", "images/random/8/jpg", "images/random/7/jpg", "d",  4],
-    [55, "Which of the following images contains fruit?", 'images/dogs/18.jpg', '', 'images/dogs/17.jpg', 'images/dogs/16.jpg', "images/dogs/12.jpg", "images/random//jpg", "images/random//jpg", "images/random//jpg", "e",  4],
-    [56, "Which of the following images contains fruit?", 'images/dogs/18.jpg', '', 'images/dogs/17.jpg', 'images/dogs/16.jpg', "images/random/4/jpg", "images/fruit/fruit1.jpg", "images/random/6/jpg", "images/random/5/jpg", "f",  4],
-    [57, "Which of the following images contains fruit?", 'images/dogs/3.jpg', 'images/dogs/15.jpg', '', 'images/dogs/13.jpg', "images/random/3/jpg", "images/random/18/jpg", "images/fruit/fruit2.jpg", "images/random//jpg", "g",  4],
-    [58, "Which of the following images contains fruit?", 'images/wrongImage/1.1.jpg', 'images/wrongImage/2.1.jpg', 'images/wrongImage/3.1.jpg', '', "images/random/14/jpg", "images/random/12/jpg", "images/random/10/jpg", "images/fruit/fruit3.jpg", "h",  4],
+    [51, "Which of the following images contains a bike?", 'images/dogs/13.jpg', 'images/dogs/11.jpg', 'images/dogs/12.jpg', 'images/dogs/10.jpg', "images/random/1.jpg", "images/random/3.jpg", "images/random/5.jpg", "images/random/7.jpg", "a", 4],
+    [52, "Which of the following images contains a bike?", 'images/dogs/18.jpg', 'images/bikes/bike1.jpg', 'images/dogs/17.jpg', 'images/dogs/16.jpg', "images/random/18.jpg", "images/random/17.jpg", "images/random/16.jpg", "images/random/15.jpg", "b",  4],
+    [53, "Which of the following images contains a bike?", 'images/dogs/3.jpg', 'images/dogs/15.jpg', 'images/bikes/bike2.jpg', 'images/dogs/5.jpg', "images/random/14.jpg", "images/random/13.jpg", "images/random/12.jpg", "images/random/11.jpg", "c",  4],
+    [54, "Which of the following images contains a bike?", 'images/dogs/12.jpg', 'images/dogs/21.jpg', 'images/dogs/17.jpg', 'images/bikes/bike3.jpg', "images/random/10.jpg", "images/random/9.jpg", "images/random/8.jpg", "images/random/7.jpg", "d",  4],
+    [55, "Which of the following images contains fruit?", 'images/dogs/18.jpg', '', 'images/dogs/17.jpg', 'images/dogs/16.jpg', "images/dogs/12.jpg", "images/random/13.jpg", "images/random/14.jpg", "images/random/15.jpg", "e",  4],
+    [56, "Which of the following images contains fruit?", 'images/dogs/18.jpg', '', 'images/dogs/17.jpg', 'images/dogs/16.jpg', "images/random/4.jpg", "images/fruit/fruit1.jpg", "images/random/8.jpg", "images/random/5.jpg", "f",  4],
+    [57, "Which of the following images contains fruit?", 'images/dogs/3.jpg', 'images/dogs/15.jpg', '', 'images/dogs/13.jpg', "images/random/3.jpg", "images/random/18.jpg", "images/fruit/fruit2.jpg", "images/random/7.jpg", "g",  4],
+    [58, "Which of the following images contains fruit?", 'images/wrongImage/1.1.jpg', 'images/wrongImage/2.1.jpg', 'images/wrongImage/3.1.jpg', 'images/random/1.jpg', "images/random/14.jpg", "images/random/12.jpg", "images/random/10.jpg", "images/fruit/fruit3.jpg", "h",  4],
     [59, "Select the word that best describes this image. <br><br><img src='images/label_image/16.jpg' style='height:300px;'>", "a", "turtle", "fish", "crab", "lobster", "salamandar", "bird", "swimming thingy", "animal", 1],
     [60, "Select the word that best describes this image. <br><br><img src='images/label_image/17.jpg' style='height:300px;'>", "b", "dinner", "cookie", "plate", "drink", "food", "dessert", "restaraunt", "spoon", 1],
     [61, "Select the word that best describes this image. <br><br><img src='images/label_image/18.jpg' style='height:300px;'>", "c", "fish", "pet", "shark", "friendly", "vegan", "teeth", "smell", "ocean", 1],
@@ -224,6 +222,7 @@ function getQuestion()
                         
                         if(Questions[iCount][11] == 4 || Questions[iCount][11] == 2)
                         {
+                            document.getElementById("cap").style.width = "90%";
                             correctAnswer = Questions[iCount][10];
                             inputType = Questions[iCount][11];
                             instructions = "<h3 style='font-size:1.4em'>" + Questions[iCount][1] 
@@ -234,7 +233,7 @@ function getQuestion()
                             + "<label><input type='radio' id='a5' value='e'class='radioImage'/> <img src='" + Questions[iCount][6] +"' style='object-fit:cover; width:300px;'></label>"
                             + "<label><input type='radio' id='a6' value='f'class='radioImage'/> <img src='" + Questions[iCount][7] +"' style='object-fit:cover; width:300px;'></label>"
                             + "<label><input type='radio' id='a7' value='g'class='radioImage'/> <img src='" + Questions[iCount][8] +"' style='object-fit:cover; width:300px;'></label>"
-                            + "<label><input type='radio' id='a8' value='h'class='radioImage'/> <img src='" + Questions[iCount][9] +"' style='object-fit:cover; width:300px;'></label><br><br><input type='button' value='Submit' id='checkAcc' onClick='checkAcc()'>";
+                            + "<label><input type='radio' id='a8' value='h'class='radioImage'/> <img src='" + Questions[iCount][9] +"' style='object-fit:cover; width:300px;'></label><br><br><input type='button' value='Submit' id='checkAcc' class='mybutton' onClick='checkAcc()'>";
                             Questions[iCount][0] = 0;
                             numFound = true;
                             iWhile++;
@@ -261,7 +260,7 @@ function getQuestion()
                                 + Questions[iCount][9]
                                 + "<br><input type='radio' name='answer' id='a8' value='h'> "
                                 + Questions[iCount][10]
-                                + "<br><br><input type='button' value='Submit' id='checkAcc' onClick='checkAcc()'>";
+                                + "<br><br><input type='button' value='Submit' id='checkAcc' class='mybutton' onClick='checkAcc()'>";
                             Questions[iCount][0]= 0;
                             numFound = true;
                             iWhile++;
