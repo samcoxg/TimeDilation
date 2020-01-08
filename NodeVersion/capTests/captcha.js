838e05d07eb86773b3a92cb425cc668a2e08793f
samcoxg <50882390+samcoxg@users.noreply.github.com>
2020-01-08 12:50:24 -0700

    c

--- NodeVersion/capTests/captcha.js
+++ NodeVersion/capTests/captcha.js
@@ -499,7 +499,7 @@ function displayCap()
     }
   //7 minutes timer
     let start = Date.now(); // The current date (in miliseconds)
-    let end = start + (200); // 1000 is one second
+    let end = start + (420000); // 1000 is one second
 
     function spinWheel() 
     {
