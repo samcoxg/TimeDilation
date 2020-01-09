4ade04b905f46144810237e569e92a54093890ed
samcoxg <50882390+samcoxg@users.noreply.github.com>
2020-01-09 08:43:46 -0700

    c

--- index.js
+++ index.js
@@ -198,4 +198,13 @@ app.post
 
 let port = 8000;
 
-app.listen(port, () => console.log("Server running."));
+//app.listen(port, () => console.log("Server running."));
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
